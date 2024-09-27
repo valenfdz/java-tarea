2791cace3daa6729e8ca3d77f24df283556a0b61
vafernandezm <valentina.030402@hotmail.com>
2024-09-26 23:42:46 -0400

    Añadir MiInformacion.java

---
+++ MiInformacion.java
@@ -0,0 +1,9 @@
+public class MiInformacion {
+    public static void main(String[] args) {
+        // Información personal
+        System.out.println("Hola, mi nombre es Valentina Fernandez.");
+        System.out.println("Tengo 22 años de edad."); 
+        System.out.println("Soy de Paraguay y vivo en la ciudad de Asunción.");
+        System.out.println("Me gusta mucho escuchar musica y viajar."); 
+    }
+}
